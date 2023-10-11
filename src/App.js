7fa3e1c17803ad53bd92b32d0f255ec3e01a4952
danielito0428 <dgnavarro06@gmail.com>
2023-10-11 11:58:59 -0600

    pipeline test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and AWS CI/CD
+          Learn React and AWS CI/CD, TEST
         </a>
       </header>
     </div>
