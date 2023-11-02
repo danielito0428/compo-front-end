3d412721e6d7d9a87167260b934d21a31f16e853
danielito0428 <dgnavarro06@gmail.com>
2023-11-01 22:06:57 -0600

    Muestra litsa de marcas en pantalla

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and AWS CI/CD, TEST
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    marcas: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8080/marcas');
+    const body = await response.json();
+    this.setState({marcas: body});
+  }
+
+  render() {
+    const {marcas} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Marcas</h2>
+              {marcas.map(marca =>
+                  <div key={marca._id}>
+                    {marca.nombre_marca} ({marca.nombre_solicitante})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
+
+
+
